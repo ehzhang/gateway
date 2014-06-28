198a3581a97c7ef4a4330a276bd077c6c1539285
Edwin Zhang <ezhang@groupon.com>
2014-06-28 16:50:42 -0500

    add force ssl

--- client/modules/dashboard/dashboard.js
+++ client/modules/dashboard/dashboard.js
@@ -2,6 +2,7 @@ Template.dashboard.username = function(){
   return Meteor.user().emails[0].address;
 };
 
-Template.dashboard.events({
-
-});
+Template.dashboard.rendered = function(){
+  $(this.find('.dashboard'))
+    .transition('fade up in');
+};
